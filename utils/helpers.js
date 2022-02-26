ff016428e7af1fd49d1d79be7f798ed5a2061cd1
PamelaCleveland <pamelac021@gmail.com>
2022-02-26 13:20:56 -0500

    touch ups

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,9 +4,9 @@ module.exports = {
       },
       format_url: url => {
         return url
-            .replace('http://', '')
-            .replace('https://', '')
-            .replace('www.', '')
+            .replace('http://', "")
+            .replace('https://', "")
+            .replace('www.', "")
             .split('/')[0]
             .split('?')[0];
     },
