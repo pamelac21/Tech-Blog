6e187a6dbe8ddb00a95fc3b8ffd29e7080fc148f
PamelaCleveland <pamelac021@gmail.com>
2022-02-26 18:53:14 -0500

    prep for comment

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-//const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
@@ -82,10 +81,5 @@ router.get('/edit/:id', withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
-/*
-router.get('/new', (req, res) => {
-    res.render('new-post');
-});
-*/
 
 module.exports = router;
